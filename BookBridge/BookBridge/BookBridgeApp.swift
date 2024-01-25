46059ac5f7d56f3c804bd499792a6f69fa932241
funMango <138420832+funMango@users.noreply.github.com>
2024-01-25 09:21:27 +0900

    Setting

---
+++ BookBridge/BookBridge/BookBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookBridgeApp.swift
+//  BookBridge
+//
+//  Created by 이민호 on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BookBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookBridge/BookBridgeUITests/BookBridgeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BookBridgeUITestsLaunchTests.swift
+//  BookBridgeUITests
+//
+//  Created by 이민호 on 1/25/24.
+//
+
+import XCTest
+
+final class BookBridgeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
