99ad339a1c2fec5dce904e3f67f34ec1da639c44
2HYUNHO <soda9503@gmail.com>
2024-02-02 16:46:46 +0900

    [Feat] 카메라 모달 뷰 닫힘 기능 추가

---
+++ BookBridge/BookBridge/Views/Posting/CameraModalView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraModalView.swift
+//  BookBridge
+//
+//  Created by jmyeong on 2/2/24.
+//
+
+import SwiftUI
+
+struct CameraModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraModalView()
+}
