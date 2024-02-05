f29c236825bdea4c647c4e96ea58e98e040f54b0
GeonH0 <h2jo369@naver.com>
2024-02-05 17:54:35 +0900

    [Feat]: firestore에 정보 저장 기능 추가

--- BookBridge/BookBridge/Views/Login/KaKaoLogin/KakaoLoginView.swift
+++ BookBridge/BookBridge/Views/Login/KaKaoLogin/KakaoLoginView.swift
@@ -9,16 +9,20 @@ import SwiftUI
 
 struct KakaoLoginView: View {
     @StateObject private var viewModel = KakaoLoginViewModel()
+    @EnvironmentObject private var pathModel: PathViewModel
     
     var body: some View {
         VStack {
             if viewModel.state == .signedOut {
-                Button(action: viewModel.kakaoAuthSignIn) {
+                Button(action: {
+                    viewModel.kakaoAuthSignIn()
+                }
+                ) {
                     ZStack {
                         Image("KaKaoLogo")
                             .resizable()
                             .frame(width: 36, height: 36)
-
+                        
                         Circle()
                             .stroke(Color(hex: "D9D9D9"))
                             .frame(width: 39)
@@ -28,13 +32,18 @@ struct KakaoLoginView: View {
             } else {
                 KakaoLoginStatusView(viewModel: viewModel)
             }
+        } .onChange(of: viewModel.state) { newState in
+            if newState == .signedIn {
+                pathModel.paths.append(.home)
+            }
         }
+        
     }
 }
 
 struct KakaoLoginStatusView: View {
     @ObservedObject var viewModel: KakaoLoginViewModel
-
+    
     var body: some View {
         Button(action: viewModel.logout) {
             Text("로그아웃")
