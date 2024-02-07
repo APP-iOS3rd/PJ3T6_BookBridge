3fc93840935cc9c5645fe254d675a4f133a4f765
jonghyunBaik <pigmammam4@gmail.com>
2024-02-07 15:18:05 +0900

    [Feat] 게시글 열람 뷰 구현

--- BookBridge/BookBridge/Views/HomeTap/HomeTapView.swift
+++ BookBridge/BookBridge/Views/HomeTap/HomeTapView.swift
@@ -68,10 +68,19 @@ struct HomeTapView: View {
             case .find:             //TODO: imageLinks 부분 받아오기
                 ForEach(viewModel.findNoticeBoards) { element in
                     if element.hopeBook.isEmpty {
-                        HomeListItemView(author: "", bookmark: true, date: element.date, id: element.id, imageLinks: [], isChange: element.isChange, locate: element.noticeLocation, title: element.noticeBoardTitle)
+                        NavigationLink {
+                            PostView(noticeBoard: element)
+                        } label: {
+                            HomeListItemView(author: "", bookmark: true, date: element.date, id: element.id, imageLinks: [], isChange: element.isChange, locate: element.noticeLocation, title: element.noticeBoardTitle)
+                        }
                     } else {
                         //TODO: 나중에 썸네일 이미지, 저자 바꾸기
-                        HomeListItemView(author: element.hopeBook[0].volumeInfo.authors?[0] ?? "", bookmark: true, date: element.date, id: element.id, imageLinks: [element.hopeBook[0].volumeInfo.imageLinks?.smallThumbnail ?? ""], isChange: element.isChange, locate: element.noticeLocation, title: element.noticeBoardTitle)
+                        
+                        NavigationLink {
+                            PostView(noticeBoard: element)
+                        } label: {
+                            HomeListItemView(author: element.hopeBook[0].volumeInfo.authors?[0] ?? "", bookmark: true, date: element.date, id: element.id, imageLinks: [element.hopeBook[0].volumeInfo.imageLinks?.smallThumbnail ?? ""], isChange: element.isChange, locate: element.noticeLocation, title: element.noticeBoardTitle)
+                        }
                         
                     }
                 }
@@ -80,7 +89,11 @@ struct HomeTapView: View {
                 
             case .change:
                 ForEach(viewModel.changeNoticeBoards) { element in
-                    HomeListItemView(author: "", bookmark: true, date: element.date, id: element.id, imageLinks: element.noticeImageLink, isChange: element.isChange, locate: element.noticeLocation, title: element.noticeBoardTitle)
+                    NavigationLink {
+                        PostView(noticeBoard: element)
+                    } label: {
+                        HomeListItemView(author: "", bookmark: true, date: element.date, id: element.id, imageLinks: element.noticeImageLink, isChange: element.isChange, locate: element.noticeLocation, title: element.noticeBoardTitle)
+                    }
                 }
                 .padding(.horizontal)
                 .padding(.bottom, 10)
