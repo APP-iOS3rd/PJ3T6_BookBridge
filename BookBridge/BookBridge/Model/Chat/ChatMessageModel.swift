408a60597035731c8500c80e2e9f6c6fbdacc576
2HYUNHO <soda9503@gmail.com>
2024-02-13 18:16:39 +0900

    [Fix] ~파일 제거

--- BookBridge/BookBridge/Model/Chat/ChatMessageModel.swift
+++ BookBridge/BookBridge/Model/Chat/ChatMessageModel.swift
@@ -25,10 +25,10 @@ struct ChatMessage: Identifiable {
 }
 
 //struct ChatRoom: Codable, Hashable, Identifiable {
-//    var id: String                         //보류
-//    var userId: String                     //나
+//    var id: String                         //ForEach용
+//    var userId: String                     //나(보류)
 //    var noticeBoardId: String              //게시물 아이디
-//    var opponentId: String                 //채팅 보낸사람 아이디    받아와서 ->
+//    var opponentId: String                 //채팅 상대방 아이디
 //    var noticeBoardTitle: String           //게시물 제목
 //    var message: String?                   //최근 채팅
 //    var date: Date?                        //채팅목록 최근 시간
