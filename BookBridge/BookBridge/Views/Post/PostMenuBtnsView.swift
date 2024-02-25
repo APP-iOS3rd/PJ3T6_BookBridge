787412201c37616284be93c76aeffb8ef6fb80c5
GeonH0 <h2jo369@naver.com>
2024-02-26 00:23:05 +0900

    [Refactor]: 게시물 삭제시 Alert창 띄우기

--- BookBridge/BookBridge/Views/Post/PostMenuBtnsView.swift
+++ BookBridge/BookBridge/Views/Post/PostMenuBtnsView.swift
@@ -11,8 +11,10 @@ struct PostMenuBtnsView: View {
     @Environment(\.dismiss) private var dismiss
     @StateObject var postViewModel: PostViewModel
     @StateObject var reportVM: ReportViewModel
+    @State private var showingDeleteAlert = false
     @Binding var isPresented: Bool
     @Binding var noticeBoard: NoticeBoard
+    
     
     var body: some View {
         VStack {
@@ -65,13 +67,18 @@ struct PostMenuBtnsView: View {
                                 .padding(1)
                             
                             Button {
-                                postViewModel.deletePost(noticeBoardId: noticeBoard.id)
-                                dismiss()
+                                showingDeleteAlert = true
                             } label: {
                                 Text("삭제하기")
                                     .modifier(MenuBtnText())
                                     .foregroundStyle(Color.red)
                             }
+                            .alert("게시물을 삭제하시겠습니까?",isPresented: $showingDeleteAlert){
+                                Button("삭제", role: .destructive) {
+                                    postViewModel.deletePost(noticeBoardId: noticeBoard.id)
+                                    dismiss()
+                                }
+                                Button("취소", role: .cancel) {}                            }
                         }
                     }
                 }
