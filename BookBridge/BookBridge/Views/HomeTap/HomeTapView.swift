88c8fa2eb22c099301fbb7428e8d6e72ae113f70
GeonH0 <h2jo369@naver.com>
2024-02-28 13:39:59 +0900

    [Refactor]: 홈탭뷰 검색창 색상 변경

--- BookBridge/BookBridge/Views/HomeTap/HomeTapView.swift
+++ BookBridge/BookBridge/Views/HomeTap/HomeTapView.swift
@@ -70,7 +70,7 @@ struct HomeTapView: View {
                             .padding(.trailing, 8)
                         }
                     }
-                    .background(Color(red: 233/255, green: 233/255, blue: 233/255))
+                    .background(Color(.systemGray6))
                     .clipShape(RoundedRectangle(cornerRadius: 10))
                     .onTapGesture {
                         isOutsideXmark = true
