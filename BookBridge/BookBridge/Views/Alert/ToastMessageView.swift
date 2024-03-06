d3a7edf00a2580cb171b4366d0fa3c5fdf9d5765
jonghyunBaik <pigmammam4@gmail.com>
2024-03-06 14:09:56 +0900

    [Feat] 주소 복사 기능 추가

---
+++ BookBridge/BookBridge/Views/Alert/ToastMessageView.swift
@@ -0,0 +1,18 @@
+//
+//  ToastMessageView.swift
+//  BookBridge
+//
+//  Created by jonghyun baik on 3/6/24.
+//
+
+import SwiftUI
+
+struct ToastMessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToastMessageView()
+}
