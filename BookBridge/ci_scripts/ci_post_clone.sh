3c7b5a0c9eb8f70eb7a7d37b155f17020e3f354d
Jihunkim95 <darak551@naver.com>
2024-03-28 13:33:27 +0900

    [Feat]: Secrets.xcconfig 핸들링코드 추가

--- BookBridge/ci_scripts/ci_post_clone.sh
+++ BookBridge/ci_scripts/ci_post_clone.sh
@@ -4,6 +4,24 @@
 #  BookBridge
 #
 #  Created by 김지훈 on 2024/03/28.
-#  Xcode Cloud에 pod 파일 설치를 위한 스크립트
+
+#  Xcode Cloud에 pod 파일 설치
 brew install cocoapods
 pod install
+
+# Secrets.xcconfig 파일 생성
+echo "Creating Secrets.xcconfig file 환경변수 참조"
+## 경로 지정
+cat <<EOF > "/Volumes/workspace/repository/BookBridge/Secrets.xcconfig"
+## Naver API Keys
+naverKeyId = $(naverKeyId)
+naverKey = $(naverKey)
+
+## Kakao API Key
+KakaoAppKey = $(KakaoAppKey)
+
+## Server URL
+SERVER_URL = $(SERVER_URL)
+EOF
+
+echo "Secrets.xcconfig file created 환경변수 참조."
