15d3d0bc15bbd97010afef826ffd819071795005
Jihunkim95 <darak551@naver.com>
2024-03-28 14:29:51 +0900

    [Feat]: GoogleService-Info.plist 핸들링 코드 추가

--- BookBridge/ci_scripts/ci_post_clone.sh
+++ BookBridge/ci_scripts/ci_post_clone.sh
@@ -10,7 +10,7 @@ brew install cocoapods
 pod install
 
 # Secrets.xcconfig 파일 생성
-echo "Creating Secrets.xcconfig file 환경변수 참조"
+echo "환경변수 참조 Secrets.xcconfig file 생성시작"
 # Secrets 경로 지정
 cat <<EOF > "/Volumes/workspace/repository/BookBridge/Secrets.xcconfig"
 // Naver API Keys
@@ -24,4 +24,45 @@ KakaoAppKey = $(KakaoAppKey)
 SERVER_URL = $(SERVER_URL)
 EOF
 
-echo "Secrets.xcconfig file created 환경변수 참조."
+echo "환경변수 참조 Secrets.xcconfig file 생성완료"
+
+echo "환경변수 참조 GoogleService-Info.plist file 생성시작"
+
+# GoogleService-Info.plist 파일 생성
+cat <<EOF > "/Volumes/workspace/repository/BookBridge/GoogleService-Info.plist"
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>CLIENT_ID</key>
+    <string>$(CLIENT_ID)</string>
+    <key>REVERSED_CLIENT_ID</key>
+    <string>$(REVERSED_CLIENT_ID)</string>
+    <key>API_KEY</key>
+    <string>$(API_KEY)</string>
+    <key>GCM_SENDER_ID</key>
+    <string>$(GCM_SENDER_ID)</string>
+    <key>PLIST_VERSION</key>
+    <string>$(PLIST_VERSION)</string>
+    <key>BUNDLE_ID</key>
+    <string>$(BUNDLE_ID)</string>
+    <key>PROJECT_ID</key>
+    <string>$(PROJECT_ID)</string>
+    <key>STORAGE_BUCKET</key>
+    <string>$(STORAGE_BUCKET)</string>
+    <key>IS_ADS_ENABLED</key>
+    <$(IS_ADS_ENABLED)/>
+    <key>IS_ANALYTICS_ENABLED</key>
+    <$(IS_ANALYTICS_ENABLED)/>
+    <key>IS_APPINVITE_ENABLED</key>
+    <$(IS_APPINVITE_ENABLED)/>
+    <key>IS_GCM_ENABLED</key>
+    <$(IS_GCM_ENABLED)/>
+    <key>IS_SIGNIN_ENABLED</key>
+    <$(IS_SIGNIN_ENABLED)/>
+    <key>GOOGLE_APP_ID</key>
+    <string>$(GOOGLE_APP_ID)</string>
+</dict>
+</plist>
+EOF
+echo "환경변수 참조 GoogleService-Info.plist file 생성완료"
